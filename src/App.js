0c2f38976fe13d95c1ba71590f1d536f50871fee
SophieL-64 <sophie.lamant31@gmail.com>
2022-06-14 14:09:51 +0200

    ajout première citation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import axios from "axios";
 import { useState } from "react";
 
 function App() {
-  const [simpson, setSimpson] = useState({});
+  const [simpson, setSimpson] = useState({
+    quote:
+      "These are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will.",
+    character: "Lisa Simpson",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083",
+    characterDirection: "Right",
+  });
 
   const getSimpsons = () => {
     console.log("test");
